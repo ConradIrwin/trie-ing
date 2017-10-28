a8dc3d35ae91f4f048ba2e1f8c2064291e16310e
Conrad Irwin <conrad.irwin@gmail.com>
2017-10-27 18:01:01 -0700

    update benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,50 +2,61 @@ global.contacts = require('./contacts.json');
 var Trie = require('../index.js');
 
 var Benchmark = require('benchmark');
-var suite = new Benchmark('Populate#trie', function () {
+var suite = new Benchmark('Populate#trie', aaa = function () {
 
     global.t = new Trie();
+    global.nodeCount = 0;
 
-    contacts.feed.entry.forEach(function (data) {
-        if (!data.gd$email) {
-            return;
-        }
+    contacts.forEach(function (data) {
 
-        var email = data.gd$email[0].address.trim().toLowerCase() || "";
-        var name = (data.title.$t || email.split("@")[0].trim() || "").toLowerCase();
+        var email = (data.email || "-").toLowerCase();
+        var name = data.name.toLowerCase();
 
-        var score = new Date(data.updated.$t).getTime();
+        var score = data.score;
 
         t.add({
             key: email,
             score: score,
-            value: data
+            value: data,
+            distinct: email + name
         });
 
         t.add({
             key: name,
             score: score,
-            value: data
+            value: data,
+            distinct: email + name
         });
+
     });
 
-});
-suite.run();
-console.log(suite.toString());
-
-console.log(t.prefixSearch('rahul', {limit: 3, unique: true}));
-
-var suite = new Benchmark('LookupPrefix', function () {
-
-    t.prefixSearch('r', {limit: 3, unique: true});
 
 });
+
 suite.run();
 
 console.log(suite.toString());
+
+p = process.hrtime()
+t.prefixSearch('t', {limit: 5, unique: true})
+p2 = process.hrtime()
+console.log((p2[0] - p[0]) * 1e3 + (p2[1] - p[1]) / 1e6)
+p = process.hrtime()
+t.prefixSearch('tee', {limit: 5, unique: true})
+p2 = process.hrtime()
+console.log((p2[0] - p[0]) * 1e3 + (p2[1] - p[1]) / 1e6)
+
 var suite = new Benchmark('LookupPrefix', function () {
 
-    t.prefixSearch('r', {limit: 6, unique: true});
+    t.prefixSearch('tee', {limit: 3, unique: true});
+
+}, {setup: aaa});
+suite.run();
+
+console.log(suite.toString());
+var suite = new Benchmark('LookupPrefix', function () {
+
+    t.prefixSearch('tee', {limit: 6, unique: true});
 
 });
 suite.run();
